1b4087705f5b59b4be40d85b0de3d28acd788e3a
Sindhu Krovvidi <sindhukrovvidi@gmail.com>
2022-08-05 23:59:04 +0530

    core types

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number, printResult: boolean, text: string) {
+    const result  = n1 + n2;
+  if (printResult) console.log(text + result);
+  else console.log("No result");
+}
+
+const number1 = 5;
+const number2 = 3;
+const printResult = false;
+const text = "This is result.. "
+
+add(number1, number2, printResult, text);
+
